e639a56f2a1e49e746b9b0ebea238b048c004843
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-08-18 10:34:02 -0500

    update

--- Basics.py
+++ Basics.py
@@ -1,1 +1,15 @@
+"\n" #New line
+{"format" "and" "f"} #print concatenate variables or strings
+"\t" #command to tab a text or string
+
+s = "Es lo que dijo ella"
+en = "That's What she said"
+ak = "I should write something"
+ann = "What you wanna do"
+ryu = "For real!!!"
+
+print(f"{ak} before I go to sleep")
+print("{0} said ryuji after listen {1} from ann".format(ryu,ann))
+print("burn my dread\ntoday")
+print(s,"\t",en)
 
