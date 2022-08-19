973f2e3a28cf62d821e852977def17064035b048
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-08-19 11:07:38 -0500

    2ex_update

--- Control_flow.py
+++ Control_flow.py
@@ -5,13 +5,18 @@
 #1 first eexercise -guess the rate of your 5 star accesory with number 
 
 guess = 45
-tries = int(input("Write a number: "))
+running = True
 
-if tries == guess:
-    print("Congratulations!")
-elif tries > 45:
-    print("You need to put a less number")
+while running:
+    tries = int(input("Write a number: "))
+    if tries == guess:
+        print("Congratulations!")
+        running = False
+    elif tries > 45:
+        print("You need to put a less number")
+    else:
+        print("A higher number")
 else:
-    print("A higher number")
+    print("END LOOP")
 
 print("END")
