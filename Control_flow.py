fabfe21c44a5581bd38518c610012a0a8a63f96b
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-08-19 10:50:09 -0500

    UPDATE

--- Control_flow.py
+++ Control_flow.py
@@ -1,1 +1,18 @@
+#while
+#for 
+#if
+
+#1 first eexercise -guess the rate of your 5 star accesory with number 
+
+guess = 45
+tries = input("Write a number: ")
+
+if tries == guess:
+    print("Congratulations!")
+elif tries > 45:
+    print("You need to put a less number")
+else:
+    print("A higher number")
+
+print("END")
 
