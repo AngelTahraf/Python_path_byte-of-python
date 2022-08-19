2945d28e8be1e04d3351af6a5980a37f94f24aea
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-08-19 14:50:51 -0500

    3ex_update

--- Control_flow.py
+++ Control_flow.py
@@ -3,18 +3,26 @@
 #if
 
 #2 first exercise -guess the rate of your 5 star accesory with number 
-
+weak_point = 6
+weak_point_2 = 9
+tries = 0
+def info_data ():
+    print("1-Head\n2-RightArm\n3-LeftArm\n4-Chest\n5-Eyes")
+    print("6-Body\n7-RightLeg\n8-LeftLeg\n9-Back\n10-Foot")
 while True:
-    weak_point = 6
-    guess = int(input("Write a number from 1 to 10"))
+    info_data()
+    guess = int(input("Write a number from 1 to 10: "))
+    tries = tries + 1
     if guess == weak_point:
         print("You did it")
         break
-    elif guess > weak_point:
-        print("Your number is higher")
-        continue
-    else:
-        print("Your number is less")
+    elif tries == 3:
+        print("You're out of guesses")
+        break
+    elif guess != weak_point:
+        print("E-------RR-------OR")
+        print("Write another number")
+        print("E-------RR-------OR")
         continue
 print("END")
 
