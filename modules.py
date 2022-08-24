0f56dbe1c83a7d5b3f6f0385f394bc5de65a9ccf
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-08-24 16:14:09 -0500

    modules (2)

--- modules.py
+++ modules.py
@@ -1,2 +1,26 @@
-import sys
-print(sys.argv)
+import prueba2
+
+mayela = "Tu madrina: "
+mb = "Mario Bros: "
+def mayela_vs_mariobros():
+    print(f"{mayela} oiga don sabe por que no paso el transporte en la mañana")
+    print(f"{mb} 'procede a ignorarla' ")
+    print(f"{mb} Me hablo oiga ? ")
+    print(f"{mayela} le ando preguntado el motivo del por que sus compañeros de trabajo llegaron tarde hoy")
+    print(f"{mb} 3 minutos despues de peleas de ustedes y mis compañeros")
+    print("Bestie procede a abrir la puerta con toallita")
+
+prueba2.mario_bros_module()
+print("--------------------------")
+mayela_vs_mariobros()
+
+
+#Prueba 2---------------
+#def mario_bros_module():
+#    mb = "Mario Bros"
+#   sr = "Señora de EST"
+#    print(f"{mb}: a donde va usted ?")
+#   print(f"{sr}: a apodaca ")
+#    print(f"{mb}: yo no voy para apodaca")
+#    print(f"{sr}: ---------")
+#   print(f"{mb}: el transporte no es para ustedes")
