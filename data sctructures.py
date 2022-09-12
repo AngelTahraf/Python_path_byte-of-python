164e776047a6355cac12ec1fc7cdf8e46335de56
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-09-12 13:59:58 -0500

    update_list

--- data sctructures.py
+++ data sctructures.py
@@ -1,1 +1,11 @@
+#list 
+
+to_do = ["excel","You're BD","notions add","you're BD2"]
+
+print("How many activitys you have: ", len(to_do))
+to_do.append("Py_data")
+print(f"I selected: {to_do[4]}")
+del to_do[4]
+print(to_do)
+
 
