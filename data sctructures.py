f584bc2f6ae73a9aa9d21e6a829fd9f3e8bf0211
AngelTahraf <111313800+AngelTahraf@users.noreply.github.com>
2022-09-13 11:40:35 -0500

    update_sequence

--- data sctructures.py
+++ data sctructures.py
@@ -1,7 +1,7 @@
-#tuples
-yu_narukami = ("Izanagi","Mara","Yoshitsune") 
-akira = ("Arsene","Thor","Alice",yu_narukami)
-
-print(f"How many personas {akira} has: ", len(akira))
-print(akira)
-print(yu_narukami)
+re_list = ["RE1","RE2","RE3","RE4","RE5"]
+name = "Resident Evil 6"
+print("List number 1 to final",re_list[:])
+print("List number final to first",re_list[::-1])
+print("Print the number 3",re_list[3])
+print("List number 1 to 4",re_list[1:4])
+print("Write the name from final to 1st letter: ", name[::-1])
